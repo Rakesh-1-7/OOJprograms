0cd2901e241d2b7ffdc79acfd799681725149884
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-11-24 20:10:45 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class book
 {
   String name, author;
@@ -17,8 +18,31 @@ class book
 
   public static void main(String args[])
   {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the total no. of books : ");
+    int n = sc.nextInt();
     book b1 = new book("J K Rowling", "Harrry Potter", 600, 1000);
     System.out.println(b1.toString());
+    book b[] = new book[n];
+    for(int i = 0; i < n; i++)
+    {
+      String name, author;
+      int price, num_pages;
+      System.out.print("\nEnter the Author of the book : ");
+      author = sc.next();
+      System.out.print("\nEnter the name of the book : ");
+      name = sc.next();
+      System.out.print("\nEnter the price of the book : ");
+      price = sc.nextInt();
+      System.out.print("\nEnter the no. pages of the book : ");
+      num_pages = sc.nextInt();
+      b[i] = new book(name, author,price, num_pages);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+      System.out.println(b[i].toString());
+    }
+
   }
 }
-
