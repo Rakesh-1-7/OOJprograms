30559df5316a0e63e1ca902c8ff0c45676b2dc9a
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-12-02 10:10:35 +0530

    lab program 4(using super)

--- overriding.java
+++ overriding.java
@@ -1,8 +1,13 @@
 class Shape
 {
     double x,y;
+    Shape(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
     
-    double printArea(double x,double y)
+    double printArea()
     {
         return 0;
     }
@@ -11,8 +16,11 @@ class Shape
 
 class Triangle extends Shape
 {
-
-    double printArea(double x, double y)
+    Triangle(int h, int b)
+    {
+        super(h, b);
+    }
+    double printArea()
     {
         return x * y * 0.5;
     }
@@ -20,8 +28,12 @@ class Triangle extends Shape
 
 class Rectangle extends Shape
 {
+    Rectangle(int h, int b)
+    {
+        super(h, b);
+    }
 
-    double printArea(double x, double y)
+    double printArea()
     {
         return x * y;
     }
@@ -30,7 +42,11 @@ class Rectangle extends Shape
 class Circle extends Shape
 {
 
-    double printArea(double x, double y)
+    Circle(int h, int b)
+    {
+        super(h, b);
+    }
+    double printArea()
     {
         return 3.14*Math.pow(x,2);
     }
@@ -41,17 +57,17 @@ class overriding
     public static void main(String args[]) 
     {
         Shape r;
-        Triangle a1 = new Triangle();    
-        Rectangle a2 = new Rectangle();
-        Circle a3 = new Circle();
+        Triangle a1 = new Triangle(10,20);    
+        Rectangle a2 = new Rectangle(10,20);
+        Circle a3 = new Circle(10,1);
 
         r = a1;
-        System.out.println("Area of Triangle : " + r.printArea(10,20));
+        System.out.println("Area of Triangle : " + r.printArea());
     
         r = a2;
-        System.out.println("Area of Triangle : " + r.printArea(10,20));
+        System.out.println("Area of Triangle : " + r.printArea());
     
         r = a3;
-        System.out.println("Area of Triangle : " + r.printArea(10,1));
+        System.out.println("Area of Triangle : " + r.printArea());
     }
 }
